47e5f35c59eef9a43d692799c55cc8d1e9539c6f
david <dkg@tuta.io>
2025-09-18 17:04:41 +0200

    chore: update build process

--- packages/pdfjs-vue/tsdown.config.ts
+++ packages/pdfjs-vue/tsdown.config.ts
@@ -3,6 +3,9 @@ import Vue from 'unplugin-vue/rolldown';
 
 export default defineConfig({
     entry: ['src/index.ts'],
+    external: ['vue'],
+    target: 'es2020',
+    tsconfig: './tsconfig.app.json',
     platform: 'neutral',
     plugins: [Vue({ isProduction: true })],
     dts: { build: true, vue: true },
