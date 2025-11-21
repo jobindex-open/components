3dd27d935681e907d0355a071be6617582d4165d
david <dkg@tuta.io>
2025-11-21 12:18:26 +0100

    update vitest.config.ts

--- packages/pdf-viewer/vitest.config.ts
+++ packages/pdf-viewer/vitest.config.ts
@@ -1,38 +1,67 @@
 import { coverageConfigDefaults, defineConfig } from 'vitest/config';
-// import { playwright } from '@vitest/browser-playwright';
+import { playwright } from '@vitest/browser-playwright';
 import vue from '@vitejs/plugin-vue';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import type { PlaywrightProviderOptions } from '@vitest/browser-playwright';
+
+const playwrightEndpoint = process.env.PW_TEST_CONNECT_WS_ENDPOINT;
+const playwrightConfig: PlaywrightProviderOptions = playwrightEndpoint
+    ? {
+          connectOptions: {
+              wsEndpoint: playwrightEndpoint,
+              exposeNetwork: '<loopback>',
+          },
+      }
+    : {};
 
 export default defineConfig({
     plugins: [vue(), tsconfigPaths()],
     test: {
-        // projects: [
-        //     {
-        //         test: {
-        include: [
-            'test/unit/**/*.{test,spec}.ts',
-            'test/**/*.unit.{test,spec}.ts',
+        projects: [
+            // Unit tests
+            {
+                extends: true,
+                test: {
+                    include: [
+                        'test/unit/**/*.{test,spec}.ts',
+                        'test/**/*.unit.{test,spec}.ts',
+                    ],
+                    name: 'unit',
+                    environment: 'jsdom',
+                },
+            },
+
+            // Browser tests (jsdom)
+            {
+                extends: true,
+                test: {
+                    include: [
+                        'test/browser/**/*.{test,spec}.ts',
+                        'test/**/*.browser.{test,spec}.ts',
+                    ],
+                    name: 'browser',
+                    environment: 'jsdom',
+                },
+            },
+
+            // e2e tests (playwright)
+            {
+                extends: true,
+                test: {
+                    name: 'e2e',
+                    include: [
+                        'test/e2e/**/*.{test,spec}.ts',
+                        'test/**/*.e2e.{test,spec}.ts',
+                    ],
+                    setupFiles: ['vitest-browser-vue'],
+                    browser: {
+                        enabled: true,
+                        provider: playwright(playwrightConfig),
+                        instances: [{ browser: 'chromium' }],
+                    },
+                },
+            },
         ],
-        name: 'unit',
-        environment: 'jsdom',
-        //     },
-        // },
-        // {
-        //     test: {
-        //         include: [
-        //             'test/browser/**/*.{test,spec}.ts',
-        //             'test/**/*.browser.{test,spec}.ts',
-        //         ],
-        //         name: 'browser',
-        //         setupFiles: ['vitest-browser-vue'],
-        //         browser: {
-        //             enabled: true,
-        //             provider: playwright(),
-        //             instances: [{ browser: 'chromium' }],
-        //         },
-        //     },
-        // },
-        // ],
         coverage: {
             include: ['src/**/*.{ts,vue}'],
             exclude: [
