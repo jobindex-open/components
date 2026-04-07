34b04b81b3789081e91c56d7656da61c2ce53ef0
david <dkg@tuta.io>
2026-04-07 22:57:02 +0200

    adding new type definitions and util in common package

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -6,13 +6,23 @@ export type DeepPartial<T> = T extends object
     ? { [K in keyof T]?: DeepPartial<T[K]> }
     : T;
 
+export type HTTP_GET = 'GET';
+export type HTTP_POST = 'POST';
+export type HTTP_PUT = 'PUT';
+export type HTTP_DELETE = 'DELETE';
+export type HTTP_CONNECT = 'CONNECT';
+export type HTTP_OPTIONS = 'OPTIONS';
+export type HTTP_TRACE = 'TRACE';
+export type HTTP_PATCH = 'PATCH';
+export type HTTP_HEAD = 'HEAD';
+
 export type HTTPMethod =
-    | 'GET'
-    | 'POST'
-    | 'PUT'
-    | 'DELETE'
-    | 'CONNECT'
-    | 'OPTIONS'
-    | 'TRACE'
-    | 'PATCH'
-    | 'HEAD';
+    | HTTP_GET
+    | HTTP_POST
+    | HTTP_PUT
+    | HTTP_DELETE
+    | HTTP_CONNECT
+    | HTTP_OPTIONS
+    | HTTP_TRACE
+    | HTTP_PATCH
+    | HTTP_HEAD;
